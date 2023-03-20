f133b801b6629f414cf4fef8e949855825a59a95
luoqiang95 <luoqiang@126.com>
2023-03-20 18:37:48 +0800

    complete spider, put v2.1

--- mtest.py
+++ mtest.py
@@ -1,3 +1,20 @@
-import os
+import pandas as pd
 
-os.mknod("/home/checkmark/project/ty_topo/caida_scamper_downloader/spider/spider/files/2022/cycle-20220302/aep-ar.team-probing.c009902.20220303.warts.gz")
+a = pd.DataFrame({
+    "a": 1,
+    "b": 2,
+    "c": None
+}, index=[1])
+b = pd.DataFrame({
+    "a": 1,
+    "b": 3,
+    "c": 4
+}, index=[1])
+# c = pd.merge(a, b, on=["a"])
+c = a.append(b)
+print(c)
+# print("=" * 5)
+# print(c.drop_duplicates(subset=["a"], keep="first"))
+index = c.loc[c.b == 3].index
+print(index)
+print(c.drop(index, axis=0))
--- spider/spider/main.py
+++
@@ -1,9 +0,0 @@
-# -*- coding:utf-8 -*-
-# Author: lq
-# data:  11:21 AM
-# file: main.py
-from downloader import main
-
-if __name__ == '__main__':
-    base_url = "https://publicdata.caida.org/datasets/topology/ark/ipv4/probe-data/team-1/2022/cycle-20220302/aep-ar.team-probing.c009902.20220303.warts.gz"
-    main(base_url=base_url)
